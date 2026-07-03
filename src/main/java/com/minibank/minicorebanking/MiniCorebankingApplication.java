8d3becc02246eab1a984d1537e4e20a9fad09de6
Tarae0419 <Tarae0419@gmail.com>
2026-07-03 22:03:35 +0900

    시작

---
+++ src/main/java/com/minibank/minicorebanking/MiniCorebankingApplication.java
@@ -0,0 +1,13 @@
+package com.minibank.minicorebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCorebankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCorebankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minibank/minicorebanking/MiniCorebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibank.minicorebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCorebankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
