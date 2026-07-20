9696420610554c8ccc5fc8765711ee06ee20f5a0
Tarae0419 <Tarae0419@gmail.com>
2026-07-20 11:29:13 +0900

    Feat: 계좌 API 구현

--- src/main/java/com/minibank/minicorebanking/account/repository/AccountRepository.java
+++ src/main/java/com/minibank/minicorebanking/account/repository/AccountRepository.java
@@ -1,11 +1,14 @@
 package com.minibank.minicorebanking.account.repository;
 
+import com.minibank.minicorebanking.account.dto.AccountResponse;
 import com.minibank.minicorebanking.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository <Account, Long>{
     Boolean existsByAccountNo(String accountNo);
     Optional<Account> findByAccountNo(String accountNo);
+    List<Account> findByCustomerId(Long customerId);
 }
--- src/main/java/com/minibank/minicorebanking/account/service/AccountService.java
+++ src/main/java/com/minibank/minicorebanking/account/service/AccountService.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
@@ -37,15 +38,34 @@ public class AccountService {
         return AccountResponse.from(accountRepository.save(account));
     }
 
-        // 계좌 조회
-        @Transactional(readOnly = true)
-        public AccountResponse getAccount(String accountNo){
-            Account account = accountRepository.findByAccountNo(accountNo)
-                    .orElseThrow(() -> new BusinessException(ErrorCode.ACCOUNT_NOT_FOUND));
-            return AccountResponse.from(account);
+    // 계좌 조회
+    @Transactional(readOnly = true)
+    public AccountResponse getAccount(String accountNo){
+        Account account = accountRepository.findByAccountNo(accountNo)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ACCOUNT_NOT_FOUND));
+        return AccountResponse.from(account);
+    }
+
+    // 고객 계좌 목록 조회
+    @Transactional(readOnly = true)
+    public List<AccountResponse> getCustomerAccounts(Long customerId){
+        if (!customerRepository.existsById(customerId)){
+            throw new BusinessException(ErrorCode.CUSTOMER_NOT_FOUND);
         }
 
-        // 고객 계좌 목록 조회
+        List<Account> accounts = accountRepository.findByCustomerId(customerId);
+        return accounts.stream().map(AccountResponse::from).toList();
+    }
+
+    // 계좌 해지
+    @Transactional
+    public void closeAccount(String accountNo){
+        Account account = accountRepository.findByAccountNo(accountNo)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        account.close();
+    }
+
 
 
     public String generateAccountNo(){
